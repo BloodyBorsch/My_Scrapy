3d92c92b74458d28564c2efc2e46bbd4f6c05b03
Maks <56753588+BloodyBorsch@users.noreply.github.com>
2024-10-16 13:10:02 +0300

    Added controller with only one print

--- meat_addiction/spiders/artplast.py
+++ meat_addiction/spiders/artplast.py
@@ -1,10 +1,19 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class ArtplastSpider(scrapy.Spider):
     name = "artplast"
     allowed_domains = ["artplast.ru"]
-    start_urls = ["https://artplast.ru"]
+    start_urls = ["https://www.artplast.ru/catalog/bumazhnye-pakety/"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        links = response.xpath('//div[contains(@x-data, "price_block")]//a/@href').getall()
+
+        for link in links:
+            yield response.follow(link, callback=self.items_parse)
+
+        print(response.status, response.url)
+
+    def items_parse(self, response: HtmlResponse):
+        print()
